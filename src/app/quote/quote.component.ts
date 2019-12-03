98cc05e1b1326d2783acdb40e82dfde7ac40a515
Roseakhonya <akhonyarose@gmail.com>
2019-12-03 10:30:32 +0300

    add functionality

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+import {Quote} from '../quote';
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+  quote: Quote[] = [
+    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
+    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2020,3,14)),
+    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2020,3,14)),
+    new Quote(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2020,3,14)),
+    new Quote(5,'Solve math homework','Damn Math',new Date(2020,3,14)),
+    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2020,3,14)),
+  ];
+  toggleDetails(index){
+    this.quote[index].showDescription = !this.quote[index].showDescription;
+  }
+  @Input() quotes: Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  quoteComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+  deleteQuote(isComplete, index){
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.quote[index].name}?`)
+
+      if (toDelete){
+        this.quote.splice(index,1)
+      }
+    }
+  }
+  constructor() { }
+  addNewQuote(quote){
+    let quoteLength = this.quote.length;
+    quote.id = quoteLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quote.push(quote)
+  }
+
+  ngOnInit() {
+  }
+
+}
